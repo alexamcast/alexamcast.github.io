3bc017e4e32898fe55b98b189b94b0ed91313c4b
Alexandra Castelazo <104883099+alexamcast@users.noreply.github.com>
2024-06-10 00:09:58 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,17 @@ function myFunction(divId) {
 	  x.style.display = "none";
 	}
   }
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+  // Two images side by side
+function two() {
+	for (i = 0; i < elements.length; i++) {
+	  elements[i].style.flex = "50%";
+	}
+  }
+  
